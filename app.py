bccb883443a55e16f6c5faaea4f45cb12d457cd5
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-04-14 20:05:21 +0000

    g

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-x = st.slider('Select a value')
+x = st.slider('choisir la valeur')
 st.write(x, 'squared is', x * x)
 st.sidebar.write("### hey!")
+
+
+prompt = st.chat_input("dit de quoi! ")
+if prompt:
+    st.write(f"L'utilisateur a publier ce prompt: {prompt}")
+    
+    import streamlit as st
+
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
